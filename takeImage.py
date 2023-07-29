5d1ddfc9ed52da56b832c558dc9008c1dad30979
notsanjay24 <91773663+notsanjay24@users.noreply.github.com>
2023-07-29 16:38:25 +0530

    Add files via upload

---
+++ takeImage.py
@@ -0,0 +1,67 @@
+import csv
+import os, cv2
+import numpy as np
+import pandas as pd
+import datetime
+import time
+
+
+
+# take Image of user
+def TakeImage(l1, l2, haarcasecade_path, trainimage_path, message, err_screen,text_to_speech):
+    if (l1 == "") and (l2==""):
+        t='Please Enter the your Enrollment Number and Name.'
+        text_to_speech(t)
+    elif l1=='':
+        t='Please Enter the your Enrollment Number.'
+        text_to_speech(t)
+    elif l2 == "":
+        t='Please Enter the your Name.'
+        text_to_speech(t)
+    else:
+        try:
+            cam = cv2.VideoCapture(0)
+            detector = cv2.CascadeClassifier(haarcasecade_path)
+            Enrollment = l1
+            Name = l2
+            sampleNum = 0
+            directory = Enrollment + "_" + Name
+            path = os.path.join(trainimage_path, directory)
+            os.mkdir(path)
+            while True:
+                ret, img = cam.read()
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                faces = detector.detectMultiScale(gray, 1.4, 5)
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    sampleNum = sampleNum + 1
+                    cv2.imwrite(
+                        f"{path}\ "
+                        + Name
+                        + "_"
+                        + Enrollment
+                        + "_"
+                        + str(sampleNum)
+                        + ".jpg",
+                        gray[y : y + h, x : x + w],
+                    )
+                    cv2.imshow("Frame", img)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+                elif sampleNum > 50:
+                    break
+            cam.release()
+            cv2.destroyAllWindows()
+            row = [Enrollment, Name]
+            with open(
+                "E:\\Mini project\\FRS\\StudentDetails\\studentdetails.csv",
+                "a+") as csv_file:
+                writer = csv.writer(csv_file, delimiter=",")
+                writer.writerow(row)
+                csv_file.close()
+            res = "Images Saved for Register No:" + Enrollment + " Name:" + Name
+            message.configure(text=res)
+            text_to_speech(res)
+        except FileExistsError as F:
+            F = "Student Data already exists"
+            text_to_speech(F)
